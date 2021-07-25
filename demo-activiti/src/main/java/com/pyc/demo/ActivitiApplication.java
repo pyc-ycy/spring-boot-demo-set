6396ade947e6f7475b3c62eb36a02c57e6caa444
7wc98#14 <2923616405@qq.com>
2021-07-25 09:36:50 +0800

    Initial commit

---
+++ demo-activiti/src/main/java/com/pyc/demo/ActivitiApplication.java
@@ -0,0 +1,25 @@
+package com.pyc.demo;
+/*
+ * @product IntelliJ IDEA
+ * @project spring-boot-demo-set
+ * @file ActivitiApplication
+ * @pack com.pyc.demo
+ * @date 2021/7/25
+ * @time 9:34
+ * @author 御承扬
+ * @E-mail 2923616405@qq.com
+ **/
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author 彭友聪
+ * @date 2021/7/25
+ */
+@SpringBootApplication
+public class ActivitiApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(ActivitiApplication.class, args);
+    }
+}
---
+++ demo-activiti/src/main/java/com/pyc/demo/config/SecurityConfig.java
@@ -0,0 +1,18 @@
+package com.pyc.demo.config;
+/*
+ * @product IntelliJ IDEA
+ * @project spring-boot-demo-set
+ * @file SecurityConfig
+ * @pack com.pyc.demo.config
+ * @date 2021/7/25
+ * @time 9:36
+ * @author 御承扬
+ * @E-mail 2923616405@qq.com
+ **/
+
+/**
+ * @author 彭友聪
+ * @date 2021/7/25
+ */
+public class SecurityConfig {
+}
